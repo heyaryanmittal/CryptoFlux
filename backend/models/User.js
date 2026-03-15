94491d92edec219e48e0601307a26d6b6cbd16d3
heyaryanmittal <heyaryanmittal@gmail.com>
2026-03-16 00:52:20 +0530

    fix: remove duplicate email index in User model to resolve Mongoose warning

--- backend/models/User.js
+++ backend/models/User.js
@@ -11,7 +11,5 @@ const userSchema = new mongoose.Schema({
   }]
 }, { timestamps: true });
 
-// Add index for faster queries
-userSchema.index({ email: 1 });
 
 module.exports = mongoose.model('User', userSchema);
